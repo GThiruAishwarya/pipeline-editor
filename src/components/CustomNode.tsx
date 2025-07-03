965624d659dbc1467cc94f1e5ef81e86e55cbfe7
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:29:00 +0530

    CustomNode.tsx

--- src/components/CustomNode.tsx
+++ src/components/CustomNode.tsx
@@ -1,1 +1,15 @@
+// src/components/CustomNode.tsx
+import React from 'react';
+import { Handle, Position } from 'reactflow';
 
+const CustomNode = ({ data }: any) => {
+  return (
+    <div className="custom-node">
+      <Handle type="target" position={Position.Left} />
+      {data.label}
+      <Handle type="source" position={Position.Right} />
+    </div>
+  );
+};
+
+export default CustomNode;
