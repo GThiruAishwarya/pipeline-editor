33b00e498558c259587d0dd67b15b81321dfe789
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:26:47 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,14 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  ReactDOM.createRoot(rootEl).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
