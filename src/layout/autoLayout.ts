882b8e26d5a575c515786271fa525aa32ad4a7be
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:32:33 +0530

    autoLayout.ts

--- src/layout/autoLayout.ts
+++ src/layout/autoLayout.ts
@@ -1,1 +1,30 @@
+// src/layout/autoLayout.ts
+import dagre from 'dagre';
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+export default function autoLayout(nodes: any[], edges: any[]) {
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+  dagreGraph.setGraph({ rankdir: 'LR' });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  return nodes.map((node) => {
+    const pos = dagreGraph.node(node.id);
+    return {
+      ...node,
+      position: { x: pos.x - nodeWidth / 2, y: pos.y - nodeHeight / 2 },
+    };
+  });
+}
 
