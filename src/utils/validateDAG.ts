f5e8bd515978c7e09206f763826a914bfe61803d
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:31:54 +0530

    validateDAG.ts

--- src/utils/validateDAG.ts
+++ src/utils/validateDAG.ts
@@ -1,1 +1,40 @@
+// src/utils/validateDAG.ts
+export default function validateDAG(nodes: any[], edges: any[]): boolean {
+  if (nodes.length < 2) return false;
 
+  const adj: { [key: string]: string[] } = {};
+  for (const node of nodes) adj[node.id] = [];
+  for (const edge of edges) {
+    if (edge.source === edge.target) return false;
+    adj[edge.source].push(edge.target);
+  }
+
+  const visited: { [key: string]: boolean } = {};
+  const recStack: { [key: string]: boolean } = {};
+
+  const hasCycle = (nodeId: string): boolean => {
+    if (!visited[nodeId]) {
+      visited[nodeId] = true;
+      recStack[nodeId] = true;
+
+      for (const neighbor of adj[nodeId]) {
+        if (!visited[neighbor] && hasCycle(neighbor)) return true;
+        else if (recStack[neighbor]) return true;
+      }
+    }
+    recStack[nodeId] = false;
+    return false;
+  };
+
+  for (const node of nodes) {
+    if (hasCycle(node.id)) return false;
+  }
+
+  const connected = new Set();
+  for (const e of edges) {
+    connected.add(e.source);
+    connected.add(e.target);
+  }
+
+  return nodes.every((node) => connected.has(node.id));
+}
