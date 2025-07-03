ac43132c71f6dda985e1576217d33a711e1f472c
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:30:47 +0530

    JSONPreview.tsx

--- src/components/JSONPreview.tsx
+++ src/components/JSONPreview.tsx
@@ -1,1 +1,13 @@
+// src/components/JSONPreview.tsx
+import React from 'react';
 
+const JSONPreview = ({ nodes, edges }: { nodes: any[]; edges: any[] }) => {
+  return (
+    <div className="json-preview">
+      <strong>JSON Preview:</strong>
+      <pre>{JSON.stringify({ nodes, edges }, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default JSONPreview;
