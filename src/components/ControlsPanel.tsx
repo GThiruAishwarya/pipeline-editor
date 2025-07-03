586b49a39a05cc8fced2226b9b931c4014e923fe
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-03 09:29:35 +0530

    ControlsPanel.tsx

--- src/components/ControlsPanel.tsx
+++ src/components/ControlsPanel.tsx
@@ -1,1 +1,13 @@
+// src/components/ControlsPanel.tsx
+import React from 'react';
 
+const ControlsPanel = ({ onAddNode, onAutoLayout }: { onAddNode: () => void; onAutoLayout: () => void }) => {
+  return (
+    <>
+      <button onClick={onAddNode}>Add Node</button>
+      <button onClick={onAutoLayout}>Auto Layout</button>
+    </>
+  );
+};
+
+export default ControlsPanel;
